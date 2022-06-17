e98b13f88cd3a9f93c127d7ec98031b78c6fca31
lopatinanton <mr.lopatin.anton@yandex.ru>
2022-06-17 16:23:55 +0300

    6. Аннотация @Override [#174891 #313138]

--- src/main/java/ru/job4j/inheritance/HtmlReport.java
+++ src/main/java/ru/job4j/inheritance/HtmlReport.java
@@ -2,8 +2,8 @@ package ru.job4j.inheritance;
 
 public class HtmlReport extends TextReport {
 
-    @Override
-    public String generate(String name, String body) {
+    @Override @Override
+    public String genrate(String name, String body) {
         return "{" + System.lineSeparator() + "\t\"name\"" + " : \"" + name + "\"" + "," + System.lineSeparator()
                 + "\t\"body\"" + " : \"" + body + "\"" + System.lineSeparator() + "}";
     }
