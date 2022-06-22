bd60aba72c18aa40cffd2e21a3ff16a8f4c84463
lopatinanton <mr.lopatin.anton@yandex.ru>
2022-06-22 17:20:23 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,33 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book one = new Book("Red", 54);
+        Book two = new Book("Yellow", 34);
+        Book three = new Book("Green", 567);
+        Book four = new Book("Clean code", 345);
+        Book[] books = new Book[4];
+        books[0] = one;
+        books[1] = two;
+        books[2] = three;
+        books[3] = four;
+        for (Book bk : books) {
+            System.out.println(bk.getName() + " - " + bk.getCount() + " pages");
+        }
+        System.out.println();
+        System.out.println("Reverse one to four.");
+        Book tmp = books[0];
+        books[0] = books[3];
+        books[3] = tmp;
+        for (Book bk : books) {
+            System.out.println(bk.getName() + " - " + bk.getCount() + " pages");
+        }
+        System.out.println();
+        System.out.println("Output of a book with the name Clean code");
+        for (Book bk : books) {
+            if (bk.getCount() == four.getCount()) {
+                System.out.println(bk.getName() + " - " + bk.getCount() + " pages");
+            }
+        }
+    }
+}
