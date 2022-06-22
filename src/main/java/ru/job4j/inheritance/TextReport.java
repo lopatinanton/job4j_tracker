23253e941783c657e7734be35ae75a5eb4e310a5
lopatinanton <mr.lopatin.anton@yandex.ru>
2022-06-22 17:43:02 +0300

    6. Аннотация @Override [#174891 #313138]

--- src/main/java/ru/job4j/inheritance/TextReport.java
+++ src/main/java/ru/job4j/inheritance/TextReport.java
@@ -1,10 +1,8 @@
 package ru.job4j.inheritance;
 
-public abstract class TextReport {
+public class TextReport {
     public String generate(String name, String body) {
         return "{" + System.lineSeparator() + "\t\"name\"" + " : \"" + name + "\"" + "," + System.lineSeparator()
                 + "\t\"body\"" + " : \"" + body + "\"" + System.lineSeparator() + "}";
     }
-
-    public abstract String genreate(String name, String body);
 }
