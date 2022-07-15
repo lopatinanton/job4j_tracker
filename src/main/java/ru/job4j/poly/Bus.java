6f7dcd32639789e9bd75f729290e1304234207aa
lopatinanton <mr.lopatin.anton@yandex.ru>
2022-07-15 17:30:15 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passengers(int number) {
+
+    }
+
+    @Override
+    public double refuel(double volume, int price) {
+        return price;
+    }
+}
