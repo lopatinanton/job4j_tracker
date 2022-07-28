f8b8a05e9957f5f75bca2db3a3560efcc0359e29
lopatinanton <mr.lopatin.anton@yandex.ru>
2022-07-28 17:05:48 +0300

    0. Что такое исключение. [#3821 #326414]

--- src/main/java/ru/job4j/ex/BackArray.java
+++ src/main/java/ru/job4j/ex/BackArray.java
@@ -8,8 +8,8 @@ public class BackArray {
         int middle = names.length / 2;
         for (int index = 0; index < middle; index++) {
             String temp = names[index];
-            names[index] = names[names.length / 2 - index];
-            names[names.length / 2 - index] = temp;
+            names[index] = names[names.length - 1];
+            names[names.length - 1] = temp;
         }
         System.out.println(Arrays.toString(names));
     }
