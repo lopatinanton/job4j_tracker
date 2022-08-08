f70c0aa16bb56ec6b0d2d7f91e38dee5ac41347d
lopatinanton <mr.lopatin.anton@yandex.ru>
2022-08-08 16:09:33 +0300

    0.5. Иерархия исключений и множественный catch. [#219368 #328916]

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -7,35 +7,33 @@ public class UserStore {
         for (User user : users) {
             if (user.getUsername().equals(login)) {
                 rsl = user;
-                System.out.println("User " + user.getUsername() + " found.");
                 break;
-            } else {
-                throw new UserNotFoundException("User not found.");
             }
+        }
+        if (rsl == null) {
+            throw new UserNotFoundException("User not found.");
         }
         return rsl;
     }
 
     public static boolean validate(User user) throws UserInvalidException {
-        if (user.isValid() && user.getUsername().length() >= 3) {
-            System.out.println("User " + user.getUsername() + " passed validation.");
-            return true;
-        } else {
-            throw new UserInvalidException("User " + user.getUsername() + " found, but it did not pass validation.");
+        if (!user.isValid() || user.getUsername().length() < 3) {
+            throw new UserInvalidException("User is not valid");
         }
+        return true;
     }
 
     public static void main(String[] args) {
         User[] users = {new User("Anton", false)};
         try {
-            User user = findUser(users, "An");
+            User user = findUser(users, "Anton");
             if (validate(user)) {
                 System.out.println("This user has in access");
             }
-        } catch (UserNotFoundException i) {
-            i.printStackTrace();
         } catch (UserInvalidException ui) {
             ui.printStackTrace();
+        } catch (UserNotFoundException i) {
+            i.printStackTrace();
         }
     }
 }
