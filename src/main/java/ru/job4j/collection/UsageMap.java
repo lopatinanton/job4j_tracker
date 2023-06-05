84eed701469274d7bfdba0a2deeaf876304ecafa
lopatinanton <mr.lopatin.anton@yandex.ru>
2023-06-05 16:49:03 +0300

    0. Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("mr.anton@yandexru", "Lopatin Anton Antonovich");
+        map.put("mr.anton@yandexru", "Lopatin Anton Antonovich");
+        map.put("mr.maksim@yandexru", "Lopatin Anton Maksimovich");
+        map.put("mr.victor@yandexru", "Lopatin Anton Victorovich");
         for (Map.Entry<String, String> entry : map.entrySet()) {
             String key = entry.getKey();
             String value = entry.getValue();
