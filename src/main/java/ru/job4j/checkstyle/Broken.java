f754084b2075c181662adb99bf665257a879baf6
lopatinanton <mr.lopatin.anton@yandex.ru>
2024-05-07 16:45:05 +0300

    36. Правила Checkstyle [#504981]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -1,23 +1,23 @@
 package ru.job4j.checkstyle;
 
 public class Broken {
-    public static final String NEW_VALUE = "";
-    public int sizeOfEmpty = 10;
-    public String surname;
+    private static final String NEW_VALUE = "";
+    private int sizeOfEmpty = 10;
+    private String surname;
 
-    Broken() {
+   public Broken() {
     }
 
-    void echo() {
+   public void echo() {
     }
 
-    void media(Object obj) {
+    public void media(Object obj) {
         String name;
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g) {
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
     }
 }
